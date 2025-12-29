7a0963f33627fe83da98853c7b3bd184e3fde75f
shadahub <marcy.regina.wu.is.gay@gmail.com>
2025-12-29 17:54:00 +0300

    Initialize Discord bot with cogs loading

---
+++ bot.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+import os
+import asyncio
+
+intents = discord.Intents.all()
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'✅ Bot is online as {bot.user}')
+
+async def load_cogs():
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            await bot.load_extension(f'cogs.{filename[:-3]}')
+
+async def main():
+    async with bot:
+        await load_cogs()
+        await bot.start(os.environ['TOKEN'])
+
+asyncio.run(main())
